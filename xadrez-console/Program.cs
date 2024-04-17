0eba4f5d407badb63e58f98f08a6fe393fac9744
Thiago da Silva Rangel <slipknothiago@gmail.com>
2024-04-17 15:15:05 -0300

    Handling exceptions

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,12 +7,19 @@ class Program
 {
     static void Main()
     {
-        Board board = new Board(8, 8);
+        try
+        {
+            Board board = new Board(8, 8);
 
-        board.InsertPiece(new King(Color.Black, board), new Position(0, 0));
-        board.InsertPiece(new Rook(Color.White, board), new Position(1, 3));
-        board.InsertPiece(new King(Color.White, board), new Position(3, 1));
+            board.InsertPiece(new King(Color.Black, board), new Position(0, 0));
+            board.InsertPiece(new Rook(Color.White, board), new Position(1, 3));
+            board.InsertPiece(new King(Color.White, board), new Position(0, 7));
 
-        Screen.BoardPrinter(board);
+            Screen.BoardPrinter(board);
+        }
+        catch (BoardException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
