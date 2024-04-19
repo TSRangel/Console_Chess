5b8583fe4a3c7ecdadede8c10fbce9e96bc4ab21
Thiago da Silva Rangel <slipknothiago@gmail.com>
2024-04-19 08:15:01 -0300

    Firsts king's moviment rules

--- xadrez-console/board/Position.cs
+++ xadrez-console/board/Position.cs
@@ -1,15 +1,21 @@
 ﻿namespace board
 {
-class Position
-{
-    public int Row { get; set; }
-    public int Column { get; set; }
-
-    public Position(int row, int column) 
+    class Position
     {
-        Row = row;
-        Column = column;
-    }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public void DefineValues(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
 
         public override string ToString()
         {
--- xadrez-console/chess/King.cs
+++ xadrez-console/chess/King.cs
@@ -12,5 +12,24 @@ namespace chess
         {
             return "K";
         }
+
+        public override bool[,] ValidMoviments()
+        {
+            bool[,] boardHouses = new bool[Board.Columns, Board.Rows];
+
+            Position position = new Position(0, 0);
+
+            position.DefineValues(Position.Row - 1, Position.Column);
+            if (Board.ValidPosition(position) && CanIMove(position))
+            {
+                boardHouses[position.Row, position.Column] = true;
+            }
+        }
+
+        private bool CanIMove(Position position)
+        {
+            Piece piece = Board.Piece(position);
+            return piece == null || piece.Color != Color;
+        }
     }
 }
